6ffd497252279c384ae8a202dbe627669dc9ef5a
amit05sept <gladiatorpandey@gmail.com>
2024-01-04 13:49:03 +0530

    changing lines in app.js 2

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ mongoose
 
 app.set("view engine", "ejs");
 app.set('views', path.join(__dirname, 'views'));
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(path.join(__dirname + "/public")));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
