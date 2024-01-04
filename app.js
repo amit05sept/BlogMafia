5780cea35e66219a0db3de833a86aa95a958cb0a
amit05sept <gladiatorpandey@gmail.com>
2024-01-04 13:39:11 +0530

    changing lines in app.js

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ const express = require("express");
 const mongoose = require("mongoose");
 const blogRoutes = require("./routes/BlogRoutes");
 const { render } = require("ejs");
+const path = require('path');
 const app = express();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT||3000;
 
 //database connect  mongoDb
 const mongoUrl = `mongodb+srv://gladiatorpandey:cD0YLRAQl4z2vYyj@cluster0.dsbtzea.mongodb.net/Blogs?retryWrites=true&w=majority`;
@@ -15,7 +16,9 @@ mongoose
   .catch((err) => console.log(err));
 
 app.set("view engine", "ejs");
+app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(__dirname + "/public"));
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -27,7 +30,7 @@ app.get("/about", (req, res) => {
   res.render("about", { title: "About" });
 });
 
-app.use("/blogs", blogRoutes);
+app.use('/blogs',blogRoutes);
 
 app.use((req, res, next) => {
   const error = new Error("Not Found");
